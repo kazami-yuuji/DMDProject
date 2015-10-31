89f19e6125cba17aeae04a4a965c7d8881921de6
CohoJET <CohoJET@gmail.com>
2015-10-31 04:43:50 +0300

    Single author search without paging

--- DMDProject/Controllers/SearchController.cs
+++ DMDProject/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using DMDProject.Models;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,33 @@ namespace DMDProject.Controllers
 {
     public class SearchController : Controller
     {
-        public ActionResult Index()
+        static readonly string PASSWORD = "password";
+
+        public ActionResult Index(string searchString)
         {
-            // Test
+            searchString = HttpUtility.HtmlEncode(searchString);
+            // Opening connection
+            NpgsqlConnection connection = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=" + PASSWORD + ";Database=project;");
+            connection.Open();
+            // Find required articles
             List<Article> articles = new List<Article>();
-            Article test = new Article();
-            test.Title = "Some Title";
-            test.PublicationID = 0;
-            articles.Add(test);
+            if (searchString != string.Empty & searchString != null)
+            {
+                NpgsqlCommand command = new NpgsqlCommand("SELECT title, description, name " +
+                                                          "FROM articles AR, authors AU, authors_of_article AA " +
+                                                          "WHERE AU.name LIKE '%" + searchString + "%' " +
+                                                          "AND AA.author_id = AU.id " +
+                                                          "AND AA.article_id = AR.id ", connection);
+                NpgsqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    Article current = new Article();
+                    current.Title = (string)reader[0];
+                    current.Description = (string)reader[1];
+                    current.Author = (string)reader[2];
+                    articles.Add(current);
+                }
+            }
             return View(articles);
         }
 
